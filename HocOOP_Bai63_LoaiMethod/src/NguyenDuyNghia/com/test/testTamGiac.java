5fac7daf66ed8fb46891580761b07972f8121062
ASUS <ASUS@192.168.1.3>
2020-02-23 16:06:19 +0700

    HocOOP_Bai63_LoaiMethod

---
+++ HocOOP_Bai63_LoaiMethod/src/NguyenDuyNghia/com/test/testTamGiac.java
@@ -0,0 +1,17 @@
+package NguyenDuyNghia.com.test;
+
+import NguyenDuyNghia.com.model.TamGiac;
+
+public class testTamGiac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TamGiac tg1=new TamGiac(20, 12, 25);
+		System.out.println("Chu vi="+tg1.tinhChuVi());
+		System.out.println("Diện tích="+tg1.tinhDienTich());
+		//System.out.println("Nửa chu vi="+tg1.tinhNuaChuVi());
+		
+
+	}
+
+}
